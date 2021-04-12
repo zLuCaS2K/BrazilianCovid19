e898da0467dc4957edc78be24322b89dc5e05286
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-04-12 09:07:00 -0300

    Inicio da implementação do HomeFragment

---
+++ app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lucasprojects.braziliancovid19.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lucasprojects.braziliancovid19.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.lucasprojects.braziliancovid19.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lucasprojects.braziliancovid19.R
 
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed({
-            Toast.makeText(this, "Chamando próxima tela!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java)).also { finish() }
         }, 1500)
     }
 }
