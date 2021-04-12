728716bb21cb62f6c4e1df3c4561a782ff05e0a9
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-04-12 10:54:46 -0300

    Implementação da Expandable Bottom Bar com Navigation Component

--- app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/MainActivity.kt
+++ app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/MainActivity.kt
@@ -2,12 +2,19 @@ package com.lucasprojects.braziliancovid19.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.lucasprojects.braziliancovid19.R
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar
+import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationMain = findViewById<ExpandableBottomBar>(R.id.bottomNavigationMain)
+        val navController = findNavController(R.id.navHostFragment)
+        ExpandableBottomBarNavigationUI.setupWithNavController(bottomNavigationMain, navController)
     }
 }
