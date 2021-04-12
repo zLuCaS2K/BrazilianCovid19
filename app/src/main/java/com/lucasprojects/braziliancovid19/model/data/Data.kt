f9e1b0fcd9b7a78181de1acd98c3b1a4dd62e63e
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-04-12 11:12:34 -0300

    Implementação das classes Data e Response

---
+++ app/src/main/java/com/lucasprojects/braziliancovid19/model/data/Data.kt
@@ -0,0 +1,18 @@
+package com.lucasprojects.braziliancovid19.model.data
+
+class Data {
+
+    var uf: String? = null
+
+    var name: String? = null
+
+    var confirmeds: String? = null
+
+    var deaths: String? = null
+
+    var deathRate: String? = null
+
+    var population: Int? = null
+
+    var date: String? = null
+}
---
+++ app/src/main/java/com/lucasprojects/braziliancovid19/model/response/Response.kt
@@ -0,0 +1,7 @@
+package com.lucasprojects.braziliancovid19.model.response
+
+import com.lucasprojects.braziliancovid19.model.data.Data
+
+class Response {
+    var data: List<Data>? = null
+}
