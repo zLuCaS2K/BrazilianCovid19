e3d1909db778af9bd197d4887b259662911df0d4
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-07-25 18:25:40 -0300

    Refatoração da MainActivity e remoção Expandable Bottom Bar

--- app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/MainActivity.kt
+++ app/src/main/java/com/lucasprojects/braziliancovid19/ui/activities/MainActivity.kt
@@ -4,29 +4,31 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.lucasprojects.braziliancovid19.R
+import com.lucasprojects.braziliancovid19.databinding.ActivityMainBinding
 import com.lucasprojects.braziliancovid19.utils.Injection
-import github.com.st235.lib_expandablebottombar.ExpandableBottomBar
-import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
     private lateinit var mMainActivityViewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
         initializeMainActivityViewModel()
-        setupExpandableBottomBarNavigation()
+        setupBottomNavigation()
     }
 
     private fun initializeMainActivityViewModel() {
         mMainActivityViewModel = ViewModelProvider(this, Injection.providerViewModelFactory(application)).get(MainActivityViewModel::class.java)
     }
 
-    private fun setupExpandableBottomBarNavigation() {
-        val bottomNavigationMain = findViewById<ExpandableBottomBar>(R.id.bottomNavigationMain)
+    private fun setupBottomNavigation() {
+        val bottomNavigation = mBinding.bottomNavigation
         val navController = findNavController(R.id.navHostFragment)
-        ExpandableBottomBarNavigationUI.setupWithNavController(bottomNavigationMain, navController)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
